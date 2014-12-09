e2c8354e6064a3e98d4664808053e34acb09f41d
Jacques du Rand <jacques@pricecheck.co.za>
2014-12-09 16:02:27 +0200

    stufff

--- main.go
+++ main.go
@@ -6,26 +6,81 @@ import
  "code.google.com/p/go.net/websocket"
     "net/http"
     "log"
+    "flag"
+    "github.com/zenazn/goji"
+ /// gojiweb   "github.com/zenazn/goji/web"
+	"math/rand"
+	"time"
+		
+ "encoding/json"
 )
+
+type PeePlayer struct {
+  XPos int  
+  YPos int
+  ZPos int
+  Name string
+  Points int
+  Damage int
+  PowerUp int
+  EpocTime int64
+  Ip string
+  Score int
+  
+}
+
+
+type PeeGame  struct {
+	Id int
+	GState int
+	PlayerCount int
+	Players []PeePlayer
+	
+	
+	
+	
+}
+
+type PCmd struct {
+	GameId int
+	Cmd int
+	Timestamp int64
+}
+
+
+var pgame = PeeGame{}
 
 func webHandlerCmd(ws *websocket.Conn) {
 
 	for {
-		msg := "\nwebHandlerCmd:"
 		
-		fmt.Println("TX: " + msg+",ip="+ws.Request().RemoteAddr)
-		err := websocket.Message.Send(ws, msg)
-		if err != nil {
-			fmt.Println("Can't send")
-			break
-		}
+		//msg :=""
+		
+		fmt.Println("DEBUG:ip="+ws.Request().RemoteAddr)
+		
 
 		var reply string
-		err = websocket.Message.Receive(ws, &reply)
+		err := websocket.Message.Receive(ws, &reply)
 		if err != nil {
 			fmt.Println("Can't receive")
 			break
 		}
+		
+		rxCmd  := &PCmd{}
+	
+		json.Unmarshal([]byte(reply), &rxCmd)
+		log.Println(fmt.Sprintf("RX-CMD:GameId(%d) , CMD(%d)   ",rxCmd.GameId,rxCmd.Cmd))
+		
+		/*
+		
+		
+		err = websocket.Message.Send(ws, msg)
+		if err != nil {
+			fmt.Println("Can't send")
+			break
+		}
+		*/
+		
 		fmt.Println("RX:" + reply + ",ip="+ws.Request().RemoteAddr)
 	}  
 }
@@ -50,21 +105,39 @@ func webHandler(ws *websocket.Conn) {
 	}  
 }
 
+func RESTGameList(w http.ResponseWriter, r *http.Request) {
+   
+    str, _ := json.Marshal(pgame)
+	fmt.Fprintf(w, string(str)) 
+    
+}
+
+
+func setupGame(){
+	rand.Seed( time.Now().UTC().UnixNano())
+	pgame.Id = rand.Intn(1000000)+1
+
+}
 
 
 func main(){
-	//static file handler.
-	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("."))))
- 	http.Handle("/static", http.StripPrefix("/static", http.FileServer(http.Dir("static"))))
+	
+	setupGame()
+	
 	http.Handle("/echo", websocket.Handler(webHandler))
-	http.Handle("/cmd", websocket.Handler(webHandlerCmd))
+	http.Handle("/rest/cmd", websocket.Handler(webHandlerCmd))
+	
+	
+	goji.Get("/rest/game", RESTGameList)
+	goji.Post("/rest/game", RESTGameList)
+	
+	
 	log.Println("PeeeServer")
 
-    err := http.ListenAndServe(":8080", nil)
-    if nil != err {
-                panic(err)
-                        }
-	fmt.Println("Pee")
+	flag.Set("bind", ":8080")
+
+    goji.Serve()
+
 }
 
 
